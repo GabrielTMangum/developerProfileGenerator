c60c9c339bff1b3bef9e777f10659dddd73089ed
GabrielTMangum <gabrieltmangum@gmail.com>
2019-11-08 17:04:17 -0500

    finished the generation of the md file and got rid of the extra md files I created to help me visualize what I was creating

--- index.js
+++ index.js
@@ -19,9 +19,20 @@ inquirer
         axios.get('https://api.github.com/users/' + response.username)
             .then(information => {
                 console.log(information.data)
-                fs.writeFile(response.username + '.md', "<h1 style='color:"+ response.colorChoice + "'>" + information.data.name + "</h1> <img src=" + information.data.avatar_url + "> <p>Bio: " + information.data.bio + "</p> <p>Company: " + information.data.company + "</p> <a href='" + information.data.html_url + "'>" + information.data.name + "'s repos</a>", function(error) {
+                fs.writeFile(response.username + '.md', 
+                "<h1 style='color:"+ response.colorChoice + "'>" + information.data.name + "</h1>" + 
+                "<img src=" + information.data.avatar_url + ">" + 
+                "<p>Bio: " + information.data.bio + "</p>" + 
+                "<p>Company: " + information.data.company + "</p>" + 
+                "<a href='" + information.data.html_url + "'>" + information.data.name + "'s repos</a>" +
+                "<p>Public repos: " + information.data.public_repos + "</p>" +
+                "<p>Followers: " + information.data.followers + "</p>" +
+                "<p>Following: " + information.data.following + "</p>" +
+                "<p>Location: " + information.data.location + "</p>", function(error) {
                     if (error) {
                         console.log("error")
+                    } else {
+                        console.log("success")
                     }
                 })
             })
